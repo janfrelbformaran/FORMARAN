91705652a4954a31c4a2b21b1e3e4ba7f5e261b9
janfrel <formaranjanfrel@gmail.com>
2025-04-11 12:47:23 +0800

    test

---
+++ shoppingcart.py
@@ -0,0 +1,13 @@
+class shoppingCart:
+    def __init__(self):
+        self.items = {}
+
+    def add_item(self, item, price):
+        self.items[item] = price
+
+    def remove_item(self, item):
+        if item in self.items:
+            del self.items[item]
+
+    def get_total(self):
+        return sum(self.items.values())
