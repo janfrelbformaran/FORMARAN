3ca28d78eb4cddb2fe8462e6b08968e766230840
janfrel <formaranjanfrel@gmail.com>
2025-04-11 16:18:47 +0800

    update

--- testshoppingcart.py
+++ testshoppingcart.py
@@ -8,6 +8,7 @@ class testshoppingcart(unittest.TestCase):
         cart.add_item("apple", 1.0)
         self.assertEqual(cart.get_total(), 1.0)
 
+
     def test_remove_item(self):
         cart = shoppingcart()
         cart.add_item("apple", 1.0)
